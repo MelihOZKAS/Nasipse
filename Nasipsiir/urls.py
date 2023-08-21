0cb4da5f8df2ecc2bef50b95aa3a462330fdf692
MelihOZKAS <melihozkass@gmail.com>
2023-08-22 02:06:41 +0300

    robots.txt eklendi v7

--- Nasipsiir/urls.py
+++ Nasipsiir/urls.py
@@ -48,4 +48,5 @@ urlpatterns = [
     path('sitemap-<section>.xml', sitemap, {'sitemaps': sitemaps}),
 
 
+
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
