c736d5a43ff9b2e2ad56e1a402a093594411aab1
MelihOZKAS <melihozkass@gmail.com>
2023-08-22 00:57:44 +0300

    site Maps ekleme hata düzeltmesi

--- Nasipsiir/sitemaps.py
+++ Nasipsiir/sitemaps.py
@@ -13,7 +13,7 @@ class SiirAltKategoriSitemap(Sitemap):
         return obj.guncelleme_tarihi
 
     def location(self, obj):
-        return reverse('siir_alt_kategori_detail', args=[obj.slug])
+        return reverse('alt_kategori_detail', args=[obj.slug])
 
 class SiirlerSitemap(Sitemap):
     changefreq = "weekly"
