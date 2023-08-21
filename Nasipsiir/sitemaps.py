8b3c708c6c25d530b923218711e47ef45b38b42b
MelihOZKAS <melihozkass@gmail.com>
2023-08-22 02:43:34 +0300

    robots.txt eklendi v13

--- Nasipsiir/sitemaps.py
+++ Nasipsiir/sitemaps.py
@@ -4,11 +4,12 @@ from siirler.models import SiirAltKategori, Siirler
 from hikayeler.models import HikayeAltKategori, Hikayeler
 from sozler.models import Sozler
 from sairler.models import Sairler
+from userPanel.models import CustomUser
 
 
 class SiirAltKategoriSitemap(Sitemap):
     changefreq = "daily"
-    priority = 0.5
+    priority = 0.3
 
     def items(self):
         return SiirAltKategori.objects.all()
@@ -36,7 +37,7 @@ class SiirlerSitemap(Sitemap):
 
 class HikayeAltKategoriSitemap(Sitemap):
     changefreq = "daily"
-    priority = 0.5
+    priority = 0.3
 
     def items(self):
         return HikayeAltKategori.objects.all()
@@ -48,7 +49,7 @@ class HikayeAltKategoriSitemap(Sitemap):
 
 class HikayelerSitemap(Sitemap):
     changefreq = "daily"
-    priority = 0.5
+    priority = 0.8
 
     def items(self):
         return Hikayeler.objects.all()
@@ -61,7 +62,7 @@ class HikayelerSitemap(Sitemap):
 
 class SairlerSitemap(Sitemap):
     changefreq = "daily"
-    priority = 0.5
+    priority = 0.8
 
     def items(self):
         return Sairler.objects.all()
@@ -75,7 +76,7 @@ class SairlerSitemap(Sitemap):
 
 class SozlerSitemap(Sitemap):
     changefreq = "daily"
-    priority = 0.5
+    priority = 0.8
 
     def items(self):
         return Sozler.objects.all()
@@ -84,3 +85,14 @@ class SozlerSitemap(Sitemap):
 
     def location(self, obj):
         return reverse('sozler:soz_slug', args=[obj.slug])
+
+
+class CustomUserSitemap(Sitemap):
+    changefreq = "daily"
+    priority = 0.5
+
+    def items(self):
+        return CustomUser.objects.filter(is_active=True)
+
+    def location(self, obj):
+        return reverse('konsol:blog-yazar-detay', args=[obj.username])
