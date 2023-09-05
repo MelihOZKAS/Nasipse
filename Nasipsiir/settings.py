640e3c7dc6927d8535393bcfc9a473d92b300a3b
MelihOZKAS <melihozkass@gmail.com>
2023-09-05 21:32:22 +0300

    Seo güncellemesi 45

--- Nasipsiir/settings.py
+++ Nasipsiir/settings.py
@@ -92,16 +92,16 @@ WSGI_APPLICATION = "Nasipsiir.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 #DATABASES = {
-#    "default": env.db()
+#    "default": {
+#        "ENGINE": "django.db.backends.sqlite3",
+#        "NAME": BASE_DIR / "db.sqlite3",
+#    }
 #}
+
+DATABASES = {
+    "default": env.db()
+}
 
 
 
