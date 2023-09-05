3c19123edc86567809f34a29464347196e19830d
MelihOZKAS <melihozkass@gmail.com>
2023-09-05 16:10:06 +0300

    Seo güncellemesi 43

--- Nasipsiir/settings.py
+++ Nasipsiir/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'siirler/static'),
@@ -143,6 +143,7 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
