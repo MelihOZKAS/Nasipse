e4ccbf82fc4b581d9c17b779f86792bdae677cca
MelihOZKAS <melihozkass@gmail.com>
2023-09-05 15:48:22 +0300

    Seo güncellemesi 42

--- Nasipsiir/settings.py
+++ Nasipsiir/settings.py
@@ -92,16 +92,16 @@ WSGI_APPLICATION = "Nasipsiir.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-#DATABASES = {
-#    "default": {
-#        "ENGINE": "django.db.backends.sqlite3",
-#        "NAME": BASE_DIR / "db.sqlite3",
-#    }
-#}
-
 DATABASES = {
-    "default": env.db()
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
+
+#DATABASES = {
+#    "default": env.db()
+#}
 
 
 
@@ -134,11 +134,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'siirler/static'),
-    os.path.join(BASE_DIR, 'userPanel/static')
+    os.path.join(BASE_DIR, 'userPanel/static'),
 ]
 
 MEDIA_URL = "/media/"
