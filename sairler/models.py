6961f5b2547e07c465d0be1a01dd44f85ac697bf
MelihOZKAS <melihozkass@gmail.com>
2023-09-08 02:57:21 +0300

    düzeltme 8

--- sairler/models.py
+++ sairler/models.py
@@ -110,7 +110,7 @@ class Sairler(models.Model):
             if img.height > 300 or img.width > 300:
                 output_size = (300, 300)
                 img.thumbnail(output_size)
-                img.save(self.kapak_resmi.path, 'AVIF', quality=70)
+                img.save(self.kapak_resmi.path, 'avif', quality=70)
 
 
     class Meta:
